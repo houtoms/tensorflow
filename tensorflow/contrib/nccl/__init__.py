229475e98043fb5106aa4020bc4b2266a005ddd7
Marek Kolodziej <mkolodziej@nvidia.com>
2017-02-24 06:32:45 +0000

    Undoing internal nccl commit due to conflict with upstream nccl

--- tensorflow/contrib/nccl/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tensorflow.contrib.nccl.python.ops.nccl import all_reduce
-from tensorflow.contrib.nccl.python.ops.nccl import bcast
-from tensorflow.contrib.nccl.python.ops.nccl import clean
