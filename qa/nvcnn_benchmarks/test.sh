60af756a51e84091997d1c1ff0014227acdd6d60
Ben Barsdell <benbarsdell@gmail.com>
2017-03-31 17:41:33 -0700

    Add L2 tests for nvcnn.py

---
+++ qa/nvcnn_benchmarks/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CNN_NUM_BATCHES=300
+CNN_DISPLAY_EVERY=10
+CNN_SHARED_CONFIG=" "
+CNN_NUM_GPUS_LIST=${CNN_NUM_GPUS_LIST:-"1 2 4"}
+
+cd ..
+
+for n in ${CNN_NUM_GPUS_LIST//;/ }; do
+    python ../nvidia-examples/cnn/nvcnn.py \
+        --num_gpus=$n \
+        --model=$CNN_MODEL \
+        --batch_size=$CNN_BATCH_SIZE \
+        --num_batches=$CNN_NUM_BATCHES \
+        --display_every=$CNN_DISPLAY_EVERY \
+        ${CNN_DATA_DIR:+"--data_dir=$CNN_DATA_DIR"} \
+        $CNN_SHARED_CONFIG \
+        $CNN_CONFIG \
+        || exit; \
+done
