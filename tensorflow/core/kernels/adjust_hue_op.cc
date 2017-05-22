654421aff6544ff154e1c8fa7699ffe273bfbe96
Ben Barsdell <benbarsdell@gmail.com>
2017-05-22 14:08:57 -0700

    Revert the now-upstreamed "GPU fused adjust_hue"
    This reverts commit e45666a91453fcf303723697ff3c6df629389892.

--- tensorflow/core/kernels/adjust_hue_op.cc
+++ tensorflow/core/kernels/adjust_hue_op.cc
@@ -22,12 +22,59 @@ limitations under the License.
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/util/work_sharder.h"
-#include "tensorflow/core/kernels/adjust_hue_op.h"
 
 namespace tensorflow {
 
 typedef Eigen::ThreadPoolDevice CPUDevice;
 typedef Eigen::GpuDevice GPUDevice;
+
+class AdjustHueOpBase : public OpKernel {
+ protected:
+  AdjustHueOpBase(OpKernelConstruction* context) : OpKernel(context) {}
+
+  struct ComputeOptions {
+    const Tensor* input;
+    const Tensor* delta;
+    Tensor* output;
+    int64 channel_count;
+  };
+
+  virtual void DoCompute(OpKernelContext* context,
+                         const ComputeOptions& options) = 0;
+
+  void Compute(OpKernelContext* context) override {
+    const Tensor& input = context->input(0);
+    const Tensor& delta = context->input(1);
+    OP_REQUIRES(context, input.dims() >= 3,
+                errors::InvalidArgument("input must be at least 3-D, got shape",
+                                        input.shape().DebugString()));
+    OP_REQUIRES(context, TensorShapeUtils::IsScalar(delta.shape()),
+                errors::InvalidArgument("delta must be scalar: ",
+                                        delta.shape().DebugString()));
+    auto channels = input.dim_size(input.dims() - 1);
+    OP_REQUIRES(
+        context, channels == 3,
+        errors::InvalidArgument("input must have 3 channels but instead has ",
+                                channels, " channels."));
+
+    Tensor* output = nullptr;
+    OP_REQUIRES_OK(context,
+                   context->allocate_output(0, input.shape(), &output));
+
+    if (input.NumElements() > 0) {
+      const int64 channel_count = input.NumElements() / channels;
+      ComputeOptions options;
+      options.input = &input;
+      options.delta = &delta;
+      options.output = output;
+      options.channel_count = channel_count;
+      DoCompute(context, options);
+    }
+  }
+};
+
+template <class Device>
+class AdjustHueOp;
 
 namespace internal {
 // Helper function to convert a RGB color to H-and-V-range. H is in the range
