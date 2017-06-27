8d0fd31c396672d8d2d94f601f49856e65c64154
Ben Barsdell <benbarsdell@gmail.com>
2017-06-27 15:13:39 -0700

    Fix one more use of __CUDACC_VER__ in Eigen

--- third_party/patch_eigen_for_cuda9.sh
+++ third_party/patch_eigen_for_cuda9.sh
@@ -3,3 +3,4 @@ cp third_party/PacketMathHalf_cuda9.h        /root/.cache/bazel/_bazel_root/*/ex
 cp third_party/TensorContractionCuda_cuda9.h /root/.cache/bazel/_bazel_root/*/external/eigen_archive/unsupported/Eigen/CXX11/src/Tensor/TensorContractionCuda.h
 cp third_party/TensorReductionCuda_cuda9.h   /root/.cache/bazel/_bazel_root/*/external/eigen_archive/unsupported/Eigen/CXX11/src/Tensor/TensorReductionCuda.h
 cp third_party/Macros_cuda9.h                /root/.cache/bazel/_bazel_root/*/external/eigen_archive/Eigen/src/Core/util/Macros.h
+cp third_party/Core_cuda9                    /root/.cache/bazel/_bazel_root/*/external/eigen_archive/Eigen/Core
