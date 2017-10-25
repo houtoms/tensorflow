4cce2482d790b1db08928db83fed9f99dde850ad
Nathan Luehr <nluehr@nvidia.com>
2017-10-25 15:41:52 -0700

    Remove Python3 dependencies from self tests

--- qa/L1_self_test_xla/test.sh
+++ qa/L1_self_test_xla/test.sh
@@ -11,8 +11,6 @@ tensorflow/tools/ci_build/install/install_deb_packages.sh
 
 pip  install wheel six==1.10.0 protobuf==3.1.0 numpy==1.11.0 scipy==0.16.1 scikit-learn==0.17.1 pandas==0.18.1 psutil py-cpuinfo pylint pep8 portpicker mock
 
-pip3 install wheel six==1.10.0 protobuf==3.1.0 numpy==1.11.0 scipy==0.16.1 scikit-learn==0.17.1 pandas==0.18.1 psutil py-cpuinfo pylint pep8 portpicker
-
 tensorflow/tools/ci_build/install/install_pip_packages.sh
 tensorflow/tools/ci_build/install/install_proto3.sh
 tensorflow/tools/ci_build/install/install_auditwheel.sh
