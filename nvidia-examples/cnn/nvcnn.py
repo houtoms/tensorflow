Update to new nccl.broadcast API and WAR bug in it
- Since tf.contrib.nccl was refactored by Google, the broadcast
  and reduce (not all_reduce) functions have been broken. This commit
  WARs it in nvcnn.py until we have a fix.

nvidia-examples/cnn/nvcnn.py
@@ -490,15 +490,14 @@ def all_sync_params(tower_params, devices):
     if len(devices) == 1:
         return tf.no_op()
     sync_ops = []
-    if have_nccl and FLAGS.nccl:
+    # TODO(benbarsdell): Re-enable this once tf.contrib.nccl.broadcast is fixed
+    #                    See https://github.com/tensorflow/tensorflow/issues/15425#issuecomment-361835192
+    if False and have_nccl and FLAGS.nccl:
         for param_on_devices in zip(*tower_params):
             # Note: param_on_devices is [paramX_gpu0, paramX_gpu1, ...]
             param0 = param_on_devices[0]
-            send_op, received_tensors = nccl.broadcast(param0, devices[1:])
-            sync_ops.append(send_op)
-            for device, param, received in zip(devices[1:],
-                                               param_on_devices[1:],
-                                               received_tensors):
+            received = nccl.broadcast(param0)
+            for device, param in zip(devices[1:], param_on_devices[1:]):
                 with tf.device(device):
                     sync_op = param.assign(received)
                     sync_ops.append(sync_op)