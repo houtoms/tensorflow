9c1aa413cc3b6060b0e02c2e9ac3a934c66de9f9
Ben Barsdell <bbarsdell@nvidia.com>
2018-03-21 10:11:28 -0700

    Remove pip3 installs from py2 self tests

--- tensorflow/tools/ci_build/install/install_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_pip_packages.sh
@@ -26,14 +26,12 @@ set -e
 pip2 install wheel
 
 pip2 install virtualenv
-pip3 install virtualenv
 
 # Install six.
 pip2 install --upgrade six==1.10.0
 
 # Install absl-py.
 pip2 install --upgrade absl-py
-pip3 install --upgrade absl-py
 
 # Install werkzeug.
 pip2 install --upgrade werkzeug==0.11.10
@@ -83,8 +81,5 @@ pip2 install grpcio
 
 # Eager-to-graph execution needs astor, gast and termcolor:
 pip2 install --upgrade astor
-pip3 install --upgrade astor
 pip2 install --upgrade gast
-pip3 install --upgrade gast
 pip2 install --upgrade termcolor
-pip3 install --upgrade termcolor
