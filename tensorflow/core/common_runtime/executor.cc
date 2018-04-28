1910ecafdb7668ab6e10af5165f8125c25134767
Ben Barsdell <bbarsdell@nvidia.com>
2018-04-27 18:49:53 -0700

    Add colors and guards to NVTX ranges
    - NVTX ranges are enabled by default but can be disabled by setting the
      environment variable TF_DISABLE_NVTX_RANGES=1.

--- tensorflow/core/common_runtime/executor.cc
+++ tensorflow/core/common_runtime/executor.cc
@@ -22,8 +22,10 @@ limitations under the License.
 #include <unordered_map>
 #include <vector>
 
+#if GOOGLE_CUDA
 #include "cuda/include/cuda_profiler_api.h"
 #include "cuda/include/nvToolsExt.h"
+#endif  // GOOGLE_CUDA
 
 #include "tensorflow/core/common_runtime/costmodel_manager.h"
 #include "tensorflow/core/common_runtime/pending_counts.h"
@@ -62,6 +64,7 @@ limitations under the License.
 #include "tensorflow/core/platform/tracing.h"
 #include "tensorflow/core/platform/types.h"
 #include "tensorflow/core/util/tensor_slice_reader_cache.h"
+#include "tensorflow/core/util/env_var.h"
 
 namespace tensorflow {
 namespace {
@@ -1491,6 +1494,66 @@ struct ExecutorState::AsyncState {
   }
 };
 
+#if GOOGLE_CUDA
+namespace nvtx_helper {
+
+inline unsigned hash_string(const char* c) {
+  enum { M = 33 };
+  unsigned hash = 5381;
+  while (*c) {
+    hash = hash * M + *c++;
+  }
+  return hash;
+}
+
+inline uint32_t get_color(unsigned hash) {
+  const uint32_t colors[] = {0x00aedb, 0xa200ff, 0xf47835, 0xd41243, 0x8ec127,
+                             0xffb3ba, 0xffdfba, 0xffffba, 0xbaffc9, 0xbae1ff,
+                             0xbbcbdb, 0x9ebd9e, 0xdd855c, 0xf1e8ca, 0x745151,
+                             0x2e4045, 0x83adb5, 0xc7bbc9, 0x5e3c58, 0xbfb5b2,
+                             0xff77aa, 0xaaff77, 0x77aaff, 0xffffff, 0x000000};
+  const int ncolor = sizeof(colors) / sizeof(uint32_t);
+  return colors[hash % ncolor];
+}
+
+inline nvtxRangeId_t nvtxRangeStart(const char* msg,
+                                    uint32_t color = 0x008D9BAF,
+                                    uint32_t category = 0) {
+  nvtxEventAttributes_t attrs = {};
+  attrs.version = NVTX_VERSION;
+  attrs.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
+  attrs.colorType = NVTX_COLOR_ARGB;
+  attrs.color = color;
+  attrs.messageType = NVTX_MESSAGE_TYPE_ASCII;
+  attrs.message.ascii = msg;
+  attrs.category = category;
+  return ::nvtxRangeStartEx(&attrs);
+}
+
+inline nvtxRangeId_t nvtxRangeStart(const char* msg, const char* type,
+                                    bool set_category = true) {
+  unsigned h = hash_string(type);
+  uint32_t color = get_color(h);
+  uint32_t category = set_category ? h : 0;
+  return nvtxRangeStart(msg, color, category);
+}
+
+}  // namespace nvtx_helper
+
+// A helper function to decide whether to enable CUDA NVTX profiling ranges
+static bool NvtxRangesEnabled() {
+  static bool is_enabled = [] {
+    bool is_disabled = false;
+    TF_CHECK_OK(tensorflow::ReadBoolFromEnvVar("TF_DISABLE_NVTX_RANGES",
+                                               /*default_val=*/false,
+                                               &is_disabled));
+    return !is_disabled;
+  }();
+  return is_enabled;
+}
+
+#endif  // GOOGLE_CUDA
+
 void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
   const GraphView& gview = impl_->gview_;
   TaggedNodeSeq ready;
@@ -1558,8 +1621,15 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
       nodestats::SetScheduled(stats, scheduled_usec);
       nodestats::SetAllStart(stats);
     }
-    nvtxRangeId_t nvtx_range = nvtxRangeStartA(node->name().c_str());
 
+#if GOOGLE_CUDA
+    nvtxRangeId_t nvtx_range;
+    if (NvtxRangesEnabled()) {
+      nvtx_range = nvtx_helper::nvtxRangeStart(
+          (node->def().op() + ": " + node->name()).c_str(),
+          node->def().op().c_str());
+    }
+#endif  // GOOGLE_CUDA
 
     if (vlog_) {
       VLOG(1) << "Process node: " << id << " step " << params.step_id << " "
@@ -1592,7 +1662,11 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
         MaybeMarkCompleted(input_frame, input_iter, id);
         // Continue to process the nodes in 'inline_ready'.
         completed = NodeDone(s, item.node, ready, stats, &inline_ready);
-        nvtxRangeEnd(nvtx_range);
+#if GOOGLE_CUDA
+        if (NvtxRangesEnabled()) {
+          nvtxRangeEnd(nvtx_range);
+        }
+#endif  // GOOGLE_CUDA
         continue;
       }
 
@@ -1611,7 +1685,11 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
         AsyncState* state =
             new AsyncState(params, tagged_node, &item, first_input, stats);
 
+#if GOOGLE_CUDA
         auto done = [this, state, nvtx_range]() {
+#else
+        auto done = [this, state]() {
+#endif  // GOOGLE_CUDA
           Device* device = impl_->params_.device;
           NodeExecStatsWrapper* stats = state->stats;  // Shorthand
           Entry* first_input = state->first_input;     // Shorthand
@@ -1652,7 +1730,11 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
           }
           const bool completed =
               NodeDone(s, state->item->node, ready, stats, nullptr);
-          nvtxRangeEnd(nvtx_range);
+#if GOOGLE_CUDA
+          if (NvtxRangesEnabled()) {
+            nvtxRangeEnd(nvtx_range);
+          }
+#endif  // GOOGLE_CUDA
           delete state;
           if (completed) Finish();
         };
@@ -1702,7 +1784,11 @@ void ExecutorState::Process(TaggedNode tagged_node, int64 scheduled_usec) {
       }
       // Postprocess.
       completed = NodeDone(s, item.node, ready, stats, &inline_ready);
-      nvtxRangeEnd(nvtx_range);
+#if GOOGLE_CUDA
+      if (NvtxRangesEnabled()) {
+        nvtxRangeEnd(nvtx_range);
+      }
+#endif  // GOOGLE_CUDA
     }
   }  // while !inline_ready.empty()
 
