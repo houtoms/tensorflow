d98aad95559f838a36364759fbbb1f7fc1b8fc46
Nathan Luehr <nluehr@nvidia.com>
2018-05-29 17:41:50 -0700

    DGX-2 Fix for tensorboard L0 test

--- qa/L0_tensorboard/test.sh
+++ qa/L0_tensorboard/test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-( cmdpid=$BASHPID; (sleep 10; kill -s SIGINT $cmdpid) & exec tensorboard --logdir /tmp )
+DEVS=$(nvidia-smi -L | wc -l)
+[[ $DEVS -gt 4 ]] && DEVS=4
+export CUDA_VISIBLE_DEVICES=$(seq -s',' 0 $((DEVS-1)))
+( cmdpid=$BASHPID; (sleep 20; kill -s SIGINT $cmdpid) & exec tensorboard --logdir /tmp )
+RET=$?
+if [[ $RET -eq 0 ]]; then
+    echo Test succeeded
+else
+    echo Test failed
+fi
+exit $RET
