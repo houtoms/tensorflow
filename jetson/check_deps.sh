d06bf9e75533ce800f7d348e8221f60e54a9a4f8
Matt Conley <mconley@nvidia.com>
2018-06-23 01:26:51 +0000

    Fixed issues with dependency script

--- jetson/check_deps.sh
+++ jetson/check_deps.sh
@@ -6,28 +6,28 @@ if sudo dpkg -V build-essential 2>/dev/null; then
   echo "build-essential package is installed."
 else
   echo "build-essential package is not installed.  Installing now."
-  echo "y" | apt-get install build-essential
+  echo "y" | sudo apt-get install build-essential
 fi  
 
 if sudo dpkg -V openjdk-8-jdk 2>/dev/null; then
   echo "OpenJDK-8-jdk package is installed."
 else
   echo "OpenJDK-8-jdk package is not installed.  Installing now."
-  echo "y" | apt-get install openjdk-8-jdk
+  echo "y" | sudo apt-get install openjdk-8-jdk
 fi
 
 if sudo dpkg -V zip 2>/dev/null; then
   echo "zip package is installed."
 else
   echo "zip package is not installed.  Installing now."
-  echo "y" | apt-get install zip
+  echo "y" | sudo apt-get install zip
 fi
 
 if sudo dpkg -V python-pip 2>/dev/null; then
   echo "Python-pip package is installed."
 else
   echo "Python-pip package is not installed.  Installing now."
-  echo "y" | apt-get install python-pip
+  echo "y" | sudo apt-get install python-pip
 fi
 
 
@@ -42,7 +42,9 @@ else
   wget https://github.com/bazelbuild/bazel/releases/download/0.13.0/bazel-0.13.0-dist.zip
   unzip bazel-0.13.0-dist.zip
   bash compile.sh
-  mv output/bazel /usr/local/bin/bazel
+  sudo mv output/bazel /usr/local/bin/bazel
+  cd $curDir
+fi
 
 #Check to see if numpy is installed
 if pip list | grep numpy 2>/dev/null; then
@@ -50,7 +52,7 @@ if pip list | grep numpy 2>/dev/null; then
 else
   echo "numpy is not installed.  Installing now."
   echo "y" | sudo pip install numpy
-
+fi
 
 #Check 
 
