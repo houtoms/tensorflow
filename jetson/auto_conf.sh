2b642b5fc96a03037d46051fd62f9e4caf80e22f
Matt Conley <mconley@nvidia.com>
2018-06-29 20:10:38 +0000

    Updated Jetson build script

---
+++ jetson/auto_conf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#This script sets Tensorflow's configuration options and runs the configure script
+export PYTHON_BIN_PATH="/usr/bin/python"
+export PYTHON_LIB_PATH="/usr/local/lib/python2.7/dist-packages"
+export TF_NEED_S3="0"
+export TF_NEED_OPENCL_SYCL="0"
+export TF_NEED_CUDA="1"
+export CUDA_TOOLKIT_PATH="/usr/local/cuda"
+export TF_CUDA_VERSION="9.0"
+export CUDNN_INSTALL_PATH="/usr/local/cuda-9.0"
+export TF_CUDNN_VERSION="7"
+export TF_NEED_TENSORRT="1"
+export TENSORRT_INSTALL_PATH="/usr/lib/aarch64-linux-gnu"
+export TF_TENSORRT_VERSION="4.0.4"
+export TF_NCCL_VERSION="1"
+export TF_CUDA_COMPUTE_CAPABILITIES="6.2"
+export TF_CUDA_CLANG="0"
+export GCC_HOST_COMPILER_PATH="/usr/bin/gcc"
+
+echo "" | ../configure
--- jetson/build_pip_whl.sh
+++ jetson/build_pip_whl.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 
+
+#NOTE:  This script must be run from the jetson/ directory
 #This script should:
-#	1) Run the configure script
-#	2) Compile tensorflow with bazel
-#	3) Build the pip whl
-#	4) Install the tensorflow package with pip
-
-#TODO automatically input the options for the configuration script:
-export PYTHON_BIN_PATH="/usr/bin/python"
-export PYTHON_LIB_PATH="/usr/local/lib/python2.7/dist-packages"
-export TF_NEED_S3="0"
-export TF_NEED_OPENCL_SYCL="0"
-export TF_NEED_CUDA="1"
-export CUDA_TOOLKIT_PATH="/usr/local/cuda"
-export TF_CUDA_VERSION="9.0"
-export CUDNN_INSTALL_PATH="/usr/local/cuda-9.0"
-export TF_CUDNN_VERSION="7"
-export TF_NEED_TENSORRT="1"
-export TENSORRT_INSTALL_PATH="/usr/lib/aarch64-linux-gnu"
-export TF_TENSORRT_VERSION="4.0.4"
-export TF_NCCL_VERSION="1"
-export TF_CUDA_COMPUTE_CAPABILITIES="6.2"
-export TF_CUDA_CLANG="0"
-export GCC_HOST_COMPILER_PATH="/usr/bin/gcc"
-
-#Result="\n\nn\nn\nn\nn\nn\nn\nn\nn\nn\ny\n9\n\n\n\ny\n\n\n6.2\nn\n\nn\n\nn\n"
-#echo -e $Result | ../configure
-
-echo "" | ../configure
+#	1) Check for and install dependencies
+#	2) Run the configure script
+#	3) Compile tensorflow with bazel
+#	4) Build the pip whl
+#	5) Install the tensorflow package with pip
 
 
+#Install required dependencies
+sudo -H bash check_deps.sh
 
+#Set configuration options and run configure script
+sudo -H bash auto_conf.sh
+
+#Compile and link tensorflow with bazel
 bazel build --config=opt --config=cuda ../tensorflow/tools/pip_package/build_pip_package
 #TODO check result of build command, if the build failed throw error
 
-bash ../tensorflow/tools/pip_package/build_pip_package.sh /tmp/tensorflow_pkg
+#Build the pip whl from the main Tensorflow directory
+FINAL_WHL_BUILD_PATH=${FINAL_WHL_BUILD_PATH:-/tmp/tensorflow_pkg}
+cd ../
+bash tensorflow/tools/pip_package/build_pip_package.sh $FINAL_WHL_BUILD_PATH 
+cd jetson/
 
-
-#TODO check if tensorflow is installed, act based thereon
+#TODO check if tensorflow is installed, uninstall if so
+#Install the Tensorflow package
 sudo -H pip install /tmp/tensorflow_pkg/*
-
 
 
 
