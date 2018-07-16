aea8fc3fd51342356c88f8348572e7872460998c
Nathan Luehr <nluehr@nvidia.com>
2018-07-16 16:21:27 -0700

    Re-enable L0_seq2seq speech2text_ds2 test
    This was likely failing due to a race condition in the R400 driver.
    [skip ci]

--- qa/L0_seq2seq/test.sh
+++ qa/L0_seq2seq/test.sh
@@ -3,17 +3,5 @@
 set -e
 cd /opt/tensorflow/nvidia-examples/OpenSeq2Seq
 
-#python -m unittest discover -s open_seq2seq -p '*_test.py'
+python -m unittest discover -s open_seq2seq -p '*_test.py'
 
-# Ugly last minute hack to disable speech2text_ds2 tests.
-python -m unittest discover -s open_seq2seq -p 'speech_utils_test.py'
-python -m unittest discover -s open_seq2seq -p 'text2text_test.py'
-python -m unittest discover -s open_seq2seq -p 'sequence_loss_test.py'
-python -m unittest discover -s open_seq2seq -p 'speech2text_test.py'
-python -m unittest discover -s open_seq2seq -p 'text2text_test.py'
-#python -m unittest discover -s open_seq2seq -p 'speech2text_ds2_test.py'
-python -m unittest discover -s open_seq2seq -p 'speech2text_w2l_test.py'
-python -m unittest discover -s open_seq2seq -p 'mp_wrapper_test.py'
-python -m unittest discover -s open_seq2seq -p 'optimizers_test.py'
-python -m unittest discover -s open_seq2seq -p 'beam_search_test.py'
-python -m unittest discover -s open_seq2seq -p 'utils_test.py'
