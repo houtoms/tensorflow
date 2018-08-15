69aa7859fd9a1919fb0cc7d0d4882a4e1adeb15c
Pooya Davoodi <pooyadavoodi@gmail.com>
2018-08-15 16:06:57 -0700

    TFTRT: remove requirement.sh from examples
    requirement.sh uses directories from qa which we don't ship.

--- qa/L1_inference_tftrt_fp16/test.sh
+++ qa/L1_inference_tftrt_fp16/test.sh
@@ -3,18 +3,21 @@
 set -e
 set -v
 
-###################### TF_TRT FP16 INFERENCE TESTS #####################
+pip install requests
+MODELS="/opt/tensorflow/qa/third_party/tensorflow_models/"
+export PYTHONPATH="$PYTHONPATH:$MODELS"
+pushd $MODELS/research/slim
+python setup.py install
+popd
 
-OUTPUT_PATH=$PATH
+###################### TF_TRT FP16 INFERENCE TESTS #####################
+OUTPUT_PATH=$PWD
 pushd ../../nvidia-examples/tftrt/scripts
-bash requirement.sh
-export PYTHONPATH=$PYTHONPATH:$PWD/models
 
 models=( mobilenet_v1 mobilenet_v2 nasnet_large nasnet_mobile resnet_v1_50 resnet_v2_50 vgg_16 vgg_19 inception_v3 inception_v4 )
-
 for i in "${models[@]}"
 do
   python -u inference.py --model $i --use_trt --precision fp16  2>&1 | tee $OUTPUT_PATH/output_tftrt_fp16_$i
-  python check_accuracy.py --tolerance 0.01 --model $i --input $OUTPUT_PATH/output_tftrt_fp16_$i
+  python -u check_accuracy.py --tolerance 0.01 --model $i --input $OUTPUT_PATH/output_tftrt_fp16_$i
 done
 popd
