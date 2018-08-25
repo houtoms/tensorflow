03afd482d9b7ab933b92a287e41a429c2fc9ac59
Pooya Davoodi <pdavoodi@nvidia.com>
2018-08-25 16:02:01 -0700

    TFTRT: add benchmark to scripts incl synthetic data [skip_ci]

--- nvidia-examples/tftrt/scripts/classification.py
+++ nvidia-examples/tftrt/scripts/classification.py
@@ -26,72 +26,85 @@ class NetDef:
         self.postprocess = postprocess
         self.model = model
         self.num_classes = num_classes
-        
-# Model definitions
-NETS = {
-    'mobilenet_v1': NetDef(
-        name='mobilenet_v1',
-        url='http://download.tensorflow.org/models/mobilenet_v1_2018_02_22/mobilenet_v1_1.0_224.tgz',
-        checkpoint_name='mobilenet_v1_1.0_224.ckpt'),
 
-    'mobilenet_v2': NetDef(
-        name='mobilenet_v2_140',
-        url='https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz',
-        checkpoint_name='mobilenet_v2_1.4_224.ckpt'),
+    def get_input_dims(self):
+        return self.input_width, self.input_height
 
-    'nasnet_mobile': NetDef(
-        name='nasnet_mobile',
-        url='https://storage.googleapis.com/download.tensorflow.org/models/nasnet-a_mobile_04_10_2017.tar.gz',
-        checkpoint_name='model.ckpt'),
+    def get_num_classes(self):
+        return self.num_classes
 
-    'nasnet_large': NetDef(
-        name='nasnet_large',
-        url='https://storage.googleapis.com/download.tensorflow.org/models/nasnet-a_large_04_10_2017.tar.gz',
-        checkpoint_name='model.ckpt',
-        input_size=331),
+def get_netdef(model):
+    """
+    Creates the dictionary NETS with model names as keys and NetDef as values.
+    Returns the NetDef corresponding to the model specified in the parameter.
 
-    'resnet_v1_50': NetDef(
-        name='resnet_v1_50',
-        url='http://download.tensorflow.org/models/official/20180601_resnet_v1_imagenet_checkpoint.tar.gz',
-        checkpoint_name=os.path.join('20180601_resnet_v1_imagenet_checkpoint', 'model.ckpt-257706'),
-        slim=False,
-        preprocess='vgg',
-        model=official.resnet.imagenet_main.ImagenetModel(resnet_size=50, resnet_version=1)),
+    model: string, the model name (see NETS table)
+    """
+    NETS = {
+        'mobilenet_v1': NetDef(
+            name='mobilenet_v1',
+            url='http://download.tensorflow.org/models/mobilenet_v1_2018_02_22/mobilenet_v1_1.0_224.tgz',
+            checkpoint_name='mobilenet_v1_1.0_224.ckpt'),
 
-    'resnet_v2_50': NetDef(
-        name='resnet_v2_50',
-        url='http://download.tensorflow.org/models/official/20180601_resnet_v2_imagenet_checkpoint.tar.gz',
-        checkpoint_name=os.path.join('20180601_resnet_v2_imagenet_checkpoint', 'model.ckpt-258931'),
-        slim=False,
-        preprocess='vgg',
-        model=official.resnet.imagenet_main.ImagenetModel(resnet_size=50, resnet_version=2)),
+        'mobilenet_v2': NetDef(
+            name='mobilenet_v2_140',
+            url='https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz',
+            checkpoint_name='mobilenet_v2_1.4_224.ckpt'),
 
-    'vgg_16': NetDef(
-        name='vgg_16',
-        url='http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz',
-        checkpoint_name='vgg_16.ckpt',
-        preprocess='vgg',
-        num_classes=1000),
+        'nasnet_mobile': NetDef(
+            name='nasnet_mobile',
+            url='https://storage.googleapis.com/download.tensorflow.org/models/nasnet-a_mobile_04_10_2017.tar.gz',
+            checkpoint_name='model.ckpt'),
 
-    'vgg_19': NetDef(
-        name='vgg_19',
-        url='http://download.tensorflow.org/models/vgg_19_2016_08_28.tar.gz',
-        checkpoint_name='vgg_19.ckpt',
-        preprocess='vgg',
-        num_classes=1000),
+        'nasnet_large': NetDef(
+            name='nasnet_large',
+            url='https://storage.googleapis.com/download.tensorflow.org/models/nasnet-a_large_04_10_2017.tar.gz',
+            checkpoint_name='model.ckpt',
+            input_size=331),
 
-    'inception_v3': NetDef(
-        name='inception_v3',
-        url='http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz',
-        checkpoint_name='inception_v3.ckpt',
-        input_size=299),
+        'resnet_v1_50': NetDef(
+            name='resnet_v1_50',
+            url='http://download.tensorflow.org/models/official/20180601_resnet_v1_imagenet_checkpoint.tar.gz',
+            checkpoint_name=os.path.join('20180601_resnet_v1_imagenet_checkpoint', 'model.ckpt-257706'),
+            slim=False,
+            preprocess='vgg',
+            model=official.resnet.imagenet_main.ImagenetModel(resnet_size=50, resnet_version=1)),
 
-    'inception_v4': NetDef(
-        name='inception_v4',
-        url='http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz',
-        checkpoint_name='inception_v4.ckpt',
-        input_size=299),
-}
+        'resnet_v2_50': NetDef(
+            name='resnet_v2_50',
+            url='http://download.tensorflow.org/models/official/20180601_resnet_v2_imagenet_checkpoint.tar.gz',
+            checkpoint_name=os.path.join('20180601_resnet_v2_imagenet_checkpoint', 'model.ckpt-258931'),
+            slim=False,
+            preprocess='vgg',
+            model=official.resnet.imagenet_main.ImagenetModel(resnet_size=50, resnet_version=2)),
+
+        'vgg_16': NetDef(
+            name='vgg_16',
+            url='http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz',
+            checkpoint_name='vgg_16.ckpt',
+            preprocess='vgg',
+            num_classes=1000),
+
+        'vgg_19': NetDef(
+            name='vgg_19',
+            url='http://download.tensorflow.org/models/vgg_19_2016_08_28.tar.gz',
+            checkpoint_name='vgg_19.ckpt',
+            preprocess='vgg',
+            num_classes=1000),
+
+        'inception_v3': NetDef(
+            name='inception_v3',
+            url='http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz',
+            checkpoint_name='inception_v3.ckpt',
+            input_size=299),
+
+        'inception_v4': NetDef(
+            name='inception_v4',
+            url='http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz',
+            checkpoint_name='inception_v4.ckpt',
+            input_size=299),
+    }
+    return NETS[model]
 
 def _deserialize_image_record(record):
     feature_map = {
@@ -127,7 +140,8 @@ def get_preprocess_fn(model, mode='classification'):
         try:    image = tf.image.decode_jpeg(imgdata, channels=3, fancy_upscaling=False, dct_method='INTEGER_FAST')
         except: image = tf.image.decode_png(imgdata, channels=3)
         # Use model's preprocessing function
-        image = NETS[model].preprocess(image, NETS[model].input_height, NETS[model].input_width, is_training=False)
+        netdef = get_netdef(model)
+        image = netdef.preprocess(image, netdef.input_height, netdef.input_width, is_training=False)
         return image, label
 
     return process
@@ -144,21 +158,21 @@ def build_classification_graph(model):
     model: string, the model name (see NETS table)
     returns: tensorflow.GraphDef, the TensorRT compatible frozen graph
     """
-    net = NETS[model]
+    netdef = get_netdef(model)
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth = True
 
     with tf.Graph().as_default() as tf_graph:
         with tf.Session(config=tf_config) as tf_sess:
-            tf_input = tf.placeholder(tf.float32, [None, net.input_height, net.input_width, 3],name='input')
-            if net.slim:
+            tf_input = tf.placeholder(tf.float32, [None, netdef.input_height, netdef.input_width, 3], name='input')
+            if netdef.slim:
                 # TF Slim Model: get model function from nets_factory
-                network_fn = nets.nets_factory.get_network_fn(net.name, net.num_classes,
+                network_fn = nets.nets_factory.get_network_fn(netdef.name, netdef.num_classes,
                         is_training=False)
                 tf_net, tf_end_points = network_fn(tf_input)
             else:
                 # TF Official Model: get model function from NETS
-                tf_net = net.model(tf_input, training=False)
+                tf_net = netdef.model(tf_input, training=False)
 
             tf_output = tf.identity(tf_net, name='logits')
             num_classes = tf_output.get_shape().as_list()[1]
@@ -202,11 +216,11 @@ def download_classification_checkpoint(model, output_dir='.'):
     if not os.path.exists(modeldir_path):
       os.makedirs(modeldir_path)
 
-    modeltar_path = os.path.join(output_dir, os.path.basename(NETS[model].url))
+    modeltar_path = os.path.join(output_dir, os.path.basename(get_netdef(model).url))
     if not os.path.isfile(modeltar_path):
-        subprocess.call(['wget', '--no-check-certificate', NETS[model].url, '-O', modeltar_path])
+        subprocess.call(['wget', '--no-check-certificate', get_netdef(model).url, '-O', modeltar_path])
 
-    checkpoint_path = os.path.join(modeldir_path, NETS[model].checkpoint_name)
+    checkpoint_path = os.path.join(modeldir_path, get_netdef(model).checkpoint_name)
     if not os.path.isfile(checkpoint_path):
         subprocess.call(['tar', '-xzf', modeltar_path, '-C', modeldir_path])
 
---
+++ qa/L2_inference_bench_synthetic/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -v
+
+pip install requests
+MODELS="$PWD/../third_party/tensorflow_models/"
+export PYTHONPATH="$PYTHONPATH:$MODELS"
+pushd $MODELS/research/slim
+python setup.py install
+popd
+
+OUTPUT_PATH=$PWD
+pushd ../../nvidia-examples/tftrt/scripts
+
+models=(
+  mobilenet_v1
+  mobilenet_v2
+  nasnet_large
+  nasnet_mobile
+  resnet_v1_50
+  resnet_v2_50
+  vgg_16
+  vgg_19
+  inception_v3
+  inception_v4 )
+
+batch_sizes=( 1 2 4 8 16 32 64 128 )
+
+for i in ${models[@]};
+do
+  for bs in ${batch_sizes[@]};
+  do
+    echo "Testing $i batch_size $bs..."
+    common_args="--model $i --batch_size $bs --use_synthetic --num_iterations 2000"
+    python -u inference.py $common_args           --precision fp32                        2>&1 | tee $OUTPUT_PATH/output_tf_bs${bs}_fp32_$i
+    python -u inference.py $common_args --use_trt --precision fp32                        2>&1 | tee $OUTPUT_PATH/output_tftrt_${bs}_fp32_$i
+    python -u inference.py $common_args --use_trt --precision fp16                        2>&1 | tee $OUTPUT_PATH/output_tftrt_${bs}_fp16_$i
+    python -u inference.py $common_args --use_trt --precision int8 --num_calib_inputs $bs 2>&1 | tee $OUTPUT_PATH/output_tftrt_${bs}_int8_$i
+    echo "DONE testing $i batch_size $bs"
+  done
+done
+popd
