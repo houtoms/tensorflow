bc598c07eefd44faf29818e16be7063c2f6ef566
Pooya Davoodi <pdavoodi@nvidia.com>
2018-08-29 15:19:05 -0700

    TFTRT: increase batch size to 64 INT8 tests [skip ci]

--- qa/L1_inference_tftrt_int8/test.sh
+++ qa/L1_inference_tftrt_int8/test.sh
@@ -20,7 +20,7 @@ models=(
 for i in "${models[@]}"
 do
   echo "Testing $i..."
-  python -u inference.py --batch_size 1 --model $i --use_trt --precision int8 2>&1 | tee $OUTPUT_PATH/output_tftrt_int8_$i
+  python -u inference.py --batch_size 64 --model $i --use_trt --precision int8 2>&1 | tee $OUTPUT_PATH/output_tftrt_int8_$i
   python -u check_accuracy.py --tolerance 1.0 --input $OUTPUT_PATH/output_tftrt_int8_$i
   echo "DONE testing $i"
 done
