ae7f290209390ca4f3944f1208dc5d01832198cd
Trevor Morris <tmorris@nvidia.com>
2018-09-04 11:50:03 -0700

    Fix code which checks if checkpoint already exists.
    This didn't work because some checkpoints are accessed in tensorflow with their prefix (eg: 'model.ckpt'), but the files themselves are actually named like 'model.ckpt.data-00000-of000001'. tf.train.latest_checkpoint would be nice to use heere except it doesn't work.

--- nvidia-examples/tftrt/scripts/classification.py
+++ nvidia-examples/tftrt/scripts/classification.py
@@ -208,21 +208,27 @@ def download_classification_checkpoint(model, output_dir='.'):
     output_dir: string, the directory where files are downloaded to
     returns: string, path to the checkpoint file containing trained model params
     """
-    checkpoint_path = ''
 
     if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
+        os.makedirs(output_dir)
 
     modeldir_path = os.path.join(output_dir, model)
     if not os.path.exists(modeldir_path):
-      os.makedirs(modeldir_path)
+        os.makedirs(modeldir_path)
 
+    # Build path to checkpoint and see if we have it already
+    checkpoint_path = os.path.join(modeldir_path, get_netdef(model).checkpoint_name)
+    if os.path.exists(os.path.dirname(checkpoint_path)):
+        for filename in os.listdir(os.path.dirname(checkpoint_path)):
+            if filename.startswith(os.path.basename(checkpoint_path)):
+                print('Using checkpoint found at:', checkpoint_path)
+                return checkpoint_path
+  
+    # We don't have the checkpoint, need to redownload or extract
+    print('Downloading checkpoint - not found at:', checkpoint_path)
     modeltar_path = os.path.join(output_dir, os.path.basename(get_netdef(model).url))
     if not os.path.isfile(modeltar_path):
         subprocess.call(['wget', '--no-check-certificate', get_netdef(model).url, '-O', modeltar_path])
-
-    checkpoint_path = os.path.join(modeldir_path, get_netdef(model).checkpoint_name)
-    if not os.path.isfile(checkpoint_path):
-        subprocess.call(['tar', '-xzf', modeltar_path, '-C', modeldir_path])
+    subprocess.call(['tar', '-xzf', modeltar_path, '-C', modeldir_path])
 
     return checkpoint_path
