db08ecceb30c6e015ebfde53821a3ff6e16720b5
Roni Forte <rforte@nvidia.com>
2018-10-15 15:10:30 +0300

    Adding JoC RN50 L2 tests

---
+++ qa/L2_joc_RN50_trainbench_fp16/test.sh
@@ -0,0 +1,15 @@
+set -o nounset
+set -o errexit
+set -o pipefail
+
+cd /opt/tensorflow/nvidia-examples/resnet50v1.5/
+
+# Setup dataset directory
+if [ ! -d "imagenet-tfrecord" ]; then
+   ln -sf /data/imagenet/train-val-tfrecord/ imagenet-tfrecord
+fi
+
+export PYTHONPATH=/opt/tensorflow/nvidia-examples/resnet50v1.5/
+
+bash ./qa/DGX1V_trainbench_fp16.sh imagenet-tfrecord
+
