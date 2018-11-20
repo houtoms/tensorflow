9bc6dda4cf8e1c49b4d2893e228bf2e1a3449aaa
Marek <mdrozdowski@nvidia.com>
2018-11-20 02:24:12 +0000

    fix L0_inference test

--- nvidia-examples/inference/image-classification/scripts/check_accuracy.py
+++ nvidia-examples/inference/image-classification/scripts/check_accuracy.py
@@ -57,14 +57,15 @@ def check_accuracy(res, tol):
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_path', dest='input_path')
-    parser.add_argument('--model', dest='model')
-    parser.add_argument('--batch_size', dest='batch_size')
-    parser.add_argument('--precision', dest='precision')
-    parser.add_argument('--tolerance', dest='tolerance', type=float, default=0.1)
+    parser.add_argument('--input_path')
+    parser.add_argument('--model')
+    parser.add_argument('--batch_size')
+    parser.add_argument('--precision')
+    parser.add_argument('--tolerance', type=float, default=0.1)
+    parser.add_argument('--dynamic_op',  type=bool, default=False) 
     
     args = parser.parse_args()
-    filename = args.input_path + "/output_" + args.precision + "_bs" + str(args.batch_size) + "_" + args.model
+    filename = args.input_path + "/output_" + args.precision + "_bs" + str(args.batch_size) + "_" + args.model + '_' + str(args.dynamic_op)
     tolerance = args.tolerance
 
     print()
