5963d98a4a032ae3c6a41fa91eec657da789a907
Trevor Morris <tmorris@nvidia.com>
2018-11-21 11:52:02 -0800

    Query GPU memory to determine batch size. Currently only switches from 32 to 64 if memory is > 12GB

--- qa/L2_inf_image_classification_tftrt_int8/test.sh
+++ qa/L2_inf_image_classification_tftrt_int8/test.sh
@@ -45,8 +45,33 @@ set_allocator() {
 }
 
 
+# This function should be used by all of the inference tests and updated to
+# access a hashtable based on {model,precision,available_memory/machine_name}.
+# We can put it in a central location such as /opt/tensorflow/qa/inference.
+set_batch_size() {
+  BATCH_SIZE=32
+  AVAIL_MiB=$(nvidia-smi -q -d MEMORY \
+            | grep -A3 "FB Memory Usage" \
+            | grep Total \
+            | awk 'BEGIN {F=1; M=-1}
+                   {
+                       if ($4 != "MiB") {M=-1}
+                       if (F==1 || $3<M) {M=$3; F=0}
+                   }
+                   END {print M}')
+  if [[ "$AVAIL_MiB" == "-1" ]]; then
+    echo "Failed to detect GPU memory sizes"
+    exit 1
+  fi
+  if [[ "$AVAIL_MiB" -ge "12000" ]]; then
+    BATCH_SIZE=64
+  fi
+}
+
+
 set_models
 set_allocator
+set_batch_size
 
 for model in "${models[@]}"
 do
@@ -57,7 +82,7 @@ do
       --download_dir "/data/tensorflow/models" \
       --model $model \
       --use_trt \
-      --batch_size 64 \
+      --batch_size $BATCH_SIZE \
       --precision int8 \
       2>&1 | tee $OUTPUT_PATH/output_tftrt_int8_$model
   python -u check_accuracy.py --tolerance 1.0 --input $OUTPUT_PATH/output_tftrt_int8_$model
