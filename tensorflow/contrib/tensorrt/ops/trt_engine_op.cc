eb03dbaec2eb9e7d403b1154e47374dbc2ed96ec
Sami Kama <skama@nvidia.com>
2018-11-27 18:43:48 -0800

    Add missing header [ci skip]

--- tensorflow/contrib/tensorrt/ops/trt_engine_op.cc
+++ tensorflow/contrib/tensorrt/ops/trt_engine_op.cc
@@ -16,6 +16,7 @@ limitations under the License.
 #if GOOGLE_CUDA
 #if GOOGLE_TENSORRT
 
+#include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
