c5a1a1cb1b311ec4eb1a1c0218f75bc28a89b3a0
Nathan Luehr <nluehr@nvidia.com>
2018-12-05 13:01:58 -0600

    Update nccl test to use nccl from tf core.

--- qa/L1_nccl_test/test_tf_nccl_ops.py
+++ qa/L1_nccl_test/test_tf_nccl_ops.py
@@ -3,16 +3,13 @@
 """
 Notes on NCCL ops
 -----------------
-For allreduce, all ops must be executed together to avoid deadlock.
-For broadcast, send_op must be executed together with all recv ops;
-  using tf.control_dependencies([send_op]) works, but reduces performance
-  by 2x.
+All collective ops must be executed together to avoid deadlock.
 """
 from __future__ import print_function
 from builtins import range
 
 import tensorflow as tf
-from tensorflow.contrib import nccl
+from tensorflow.python.ops import nccl_ops
 
 import numpy as np
 import time
@@ -25,7 +22,7 @@ def test_allreduce(sess, devices):
 		with tf.device(device_name) as device:
 			inputs.append(tf.constant(data))
 	
-	sums = nccl.all_sum(inputs)
+	sums = nccl_ops.all_sum(inputs)
 	sum_ops = [s.op for s in sums]
 	
 	ngpu = len(devices)
@@ -49,8 +46,12 @@ def test_broadcast(sess, devices):
 	n = 4096
 	data = np.random.randint(-128, 128, size=(n,n)).astype(np.float32)
 	with tf.device(devices[0]):
-		data0 = tf.constant(data)
-	send_op, received_tensors = nccl.broadcast(data0, devices[1:])
+		data0 = tf.constant(data) bcast_data = nccl_ops.broadcast(data0)
+
+	received_tensors = []
+	for device_name in devices:
+		with tf.device(device_name):
+			received_tensors.append(tf.identity(bcast_data))
 	recv_ops = [r.op for r in received_tensors]
 	
 	ngpu = len(devices)
@@ -58,17 +59,17 @@ def test_broadcast(sess, devices):
 	nrep    = 100
 	print("Running broadcast warmup")
 	for _ in range(nwarmup):
-		sess.run([send_op] + recv_ops)
+		sess.run(recv_ops)
 	print("Running broadcast actual")
 	start_time = time.time()
 	for _ in range(nrep):
-		sess.run([send_op] + recv_ops)
+		sess.run(recv_ops)
 	elapsed_secs = time.time() - start_time
 	nbyte = data.size * data.dtype.itemsize
 	print("Broadcast aggregate BW: {} GB/s".format(nrep*ngpu*nbyte/elapsed_secs/1e9))
 	
-	results = sess.run([send_op] + received_tensors)[1:]
-	return all([np.all(result == data) for result in results])
+	results = sess.run(received_tensors)
+	return all([np.all(result == data) for result in results]) and len(results) == len(devices)
 
 def main():
 	import sys
@@ -79,14 +80,12 @@ def main():
 	devices = ['/gpu:%i' % i for i in range(ngpu)]
 	sess = tf.Session()
 	allreduce_success = test_allreduce(sess, devices)
-	# TODO(benbarsdell): Re-enable broadcast test once upstream #15425 is fixed
-	#broadcast_success = test_broadcast(sess, devices)
+	broadcast_success = test_broadcast(sess, devices)
 	print("Allreduce", "PASSED" if allreduce_success else "FAILED")
-	#print("Broadcast", "PASSED" if broadcast_success else "FAILED")
-	print("Broadcast SKIPPED (see issue #15425)")
+	print("Broadcast", "PASSED" if broadcast_success else "FAILED")
 	sess.close()
 	
-	if not allreduce_success:# or not broadcast_success:
+	if not allreduce_success or not broadcast_success:
 		sys.exit(-1)
 
 if __name__ == '__main__':
