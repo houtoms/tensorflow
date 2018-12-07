7a0873c345ec1f135103536fd7ac5e0eb162ce64
Janusz Lisiecki <jlisiecki@nvidia.com>
2018-12-07 14:02:16 -0800

    Fix build error

--- nvidia-examples/cnn/nvutils/image_processing.py
+++ nvidia-examples/cnn/nvutils/image_processing.py
@@ -251,7 +251,7 @@ class DaliPreprocessor(object):
 
         with tf.device("/gpu:0"):
             self.images, self.labels = daliop(
-                serialized_pipeline=serialized_pipe
+                serialized_pipeline=serialized_pipe,
                 shapes=[(batch_size, height, width, 3), ()],
                 dtypes=[tf.float32, tf.int64],
                 device_id=hvd.rank())
