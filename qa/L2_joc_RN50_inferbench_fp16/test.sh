d4ba0efc6fb702688028c57ee519647a8b833b4f
Pawel Soltysiak <psoltysiak@nvidia.com>
2019-02-20 17:38:27 +0100

    Fixed missing mkdir in L2_joc_RN50_inferbench_fp16/test.sh

--- qa/L2_joc_RN50_inferbench_fp16/test.sh
+++ qa/L2_joc_RN50_inferbench_fp16/test.sh
@@ -11,4 +11,6 @@ fi
 
 export PYTHONPATH=/opt/tensorflow/nvidia-examples/resnet50v1.5/
 
+mkdir -p /tmp/results
+
 python ./testscript.py --mode inference --precision fp16 --bench-warmup 100 --bench-iterations 200 --ngpus 1 --bs 1 2 4 8 16 32 64 128 256 --baseline benchmark_baselines/RN50_tensorflow_infer_fp16.json  -data_dir imagenet-tfrecord --results_dir /tmp/results
--- qa/L3_joc_RN50_convergence_fp16/test.sh
+++ qa/L3_joc_RN50_convergence_fp16/test.sh
@@ -12,4 +12,5 @@ fi
 export PYTHONPATH=/opt/tensorflow/nvidia-examples/resnet50v1.5/
 
 mkdir -p /tmp/results
+
 python ./qa/test_accuracy.py --precision fp16 --iterations 90 --ngpus 8 --bs 128 --top1-baseline 76 --top5-baseline 92 --data_dir imagenet-tfrecord --results_dir /tmp/results
