cd791f500c32052dfb52c812655ebae162f4b72f
Pooya Davoodi <pdavoodi@nvidia.com>
2019-03-01 16:52:38 -0800

    Fix test.sh for object detection
    Also fix the name of test cases for xavier

--- qa/L1_inf_object_detection/test.sh
+++ qa/L1_inf_object_detection/test.sh
@@ -15,60 +15,26 @@ pushd $PWD/../../nvidia-examples/tensorrt
 python setup.py install
 popd
 
-echo Detect arch...
-lscpu | grep -q ^Architecture:.*aarch64
-is_aarch64=$[!$?]
-lscpu | grep -q ^CPU\(s\):.*8
-is_8cpu=$[!$?]
-lscpu | grep -q ^CPU\(s\):.*4
-is_4cpu=$[!$?]
-is_nano=$[$is_aarch64 && $is_4cpu]
+echo Detecting platform...
+is_aarch64=$(lscpu | grep -q ^Architecture:.*aarch64)
+is_8cpu=$(lscpu | grep -q ^CPU\(s\):.*8)
+is_4cpu=$(lscpu | grep -q ^CPU\(s\):.*4)
 is_xavier=$[$is_aarch64 && $is_8cpu]
+is_nano=$[$is_aarch64 && $is_4cpu]
 
-echo Find all test cases...
-if [[ "$is_xavier" == 1 ]]
-then
-  TEST_CASES=(
-    faster_rcnn_resnet50_coco.json
-    ssd_inception_v2_coco_trt_fp16.json
-    ssdlite_mobilenet_v2_coco_trt_fp16.json
-    ssd_mobilenet_v1_coco_trt_fp16.json
-    ssd_mobilenet_v2_coco_trt_fp16.json
-    mask_rcnn_resnet50_atrous_coco.json
-    ssd_inception_v2_coco_trt_fp32.json
-    ssdlite_mobilenet_v2_coco_trt_fp32.json
-    ssd_mobilenet_v1_coco_trt_fp32.json
-    ssd_mobilenet_v2_coco_trt_fp32.json
-    ssd_inception_v2_coco_tf.json
-    ssdlite_mobilenet_v2_coco_tf.json
-    ssd_mobilenet_v1_coco_tf.json
-    ssd_mobilenet_v2_coco_tf.json
-    )
-  for (( i=0; i<${#array[@]}; i++ ));
-  do
-    TEST_CASES[$i]="$SCRIPTS_PATH/tests/xavier_acc_perf/${TEST_CASES[$i]}"
-  done
-elif [[ "$is_nano" == 1 ]]
-then
-  TEST_CASES=(
-    ssd_mobilenet_v2_coco_trt_fp16.json
-    ssdlite_mobilenet_v2_coco_trt_fp16.json
-    )
-  for (( i=0; i<${#array[@]}; i++ ));
-  do
-    TEST_CASES[$i]="$SCRIPTS_PATH/tests/generic_acc/${TEST_CASES[$i]}"
-  done
-else
-  TEST_CASES=(
-    #faster_rcnn_nas_tf.json
-    #faster_rcnn_nas_trt_fp16.json
-    #faster_rcnn_nas_trt_fp32.json
-    faster_rcnn_resnet50_coco_tf.json
-    #faster_rcnn_resnet50_coco_trt_fp16.json
-    #faster_rcnn_resnet50_coco_trt_fp32.json
-    mask_rcnn_resnet50_atrous_coco_tf.json
-    #mask_rcnn_resnet50_atrous_coco_trt_fp16.json
-    #mask_rcnn_resnet50_atrous_coco_trt_fp32.json
+echo "Setting test_path..."
+test_path="$SCRIPTS_PATH/tests/generic_acc/${test_case}"
+# There is a different test case dir just for xavier.
+# The only difference between this dir and generic is the performance
+# data that is specific to xavier.
+if [ ${is_xavier} == 1 ]; then
+  test_path="$SCRIPTS_PATH/tests/xavier_acc_perf/${test_case}"
+fi
+
+set_test_cases() {
+  # Name of test cases for xavier and other GPUs are the same, but
+  # they are stored in different directories.
+  test_cases=(
     ssd_inception_v2_coco_tf.json
     ssd_inception_v2_coco_trt_fp16.json
     ssd_inception_v2_coco_trt_fp32.json
@@ -81,19 +47,32 @@ else
     ssdlite_mobilenet_v2_coco_tf.json
     ssdlite_mobilenet_v2_coco_trt_fp16.json
     ssdlite_mobilenet_v2_coco_trt_fp32.json
+    faster_rcnn_resnet50_coco_tf.json
+    mask_rcnn_resnet50_atrous_coco_tf.json
+    #faster_rcnn_resnet50_coco_trt_fp16.json
+    #faster_rcnn_resnet50_coco_trt_fp32.json
+    #faster_rcnn_nas_tf.json
+    #faster_rcnn_nas_trt_fp16.json
+    #faster_rcnn_nas_trt_fp32.json
+    #mask_rcnn_resnet50_atrous_coco_trt_fp16.json
+    #mask_rcnn_resnet50_atrous_coco_trt_fp32.json
+  )
+  if [ ${is_nano} == 1 ]; then
+    test_cases=(
+      ssd_mobilenet_v2_coco_trt_fp16.json
+      ssdlite_mobilenet_v2_coco_trt_fp16.json
     )
-  for (( i=0; i<${#array[@]}; i++ ));
-  do
-    TEST_CASES[$i]="$SCRIPTS_PATH/tests/generic_acc/${TEST_CASES[$i]}"
-  done
-fi
+  fi
+}
+
+set_test_cases
 
 echo Run all tests...
 rv=0
-for test_case in "${TEST_CASES[@]}"
+for test_case in "${test_cases[@]}"
 do
   echo "Testing $test_case..."
-  python -m tftrt.examples.object_detection.test ${test_case} ; rv=$(($rv+$?))
+  python -m tftrt.examples.object_detection.test "${test_path}/${test_case}" ; rv=$(($rv+$?))
   echo "DONE testing $test_case"
 done
 exit $rv
