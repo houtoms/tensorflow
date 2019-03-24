7d9115fe1a4308ff9e9e24af5ae315719c0b3c73
Kaixi Hou <kaixih@nvidia.com>
2019-03-24 13:09:38 -0700

    add AMP support for FusedBatchNormV3

--- tensorflow/core/grappler/optimizers/auto_mixed_precision_lists.h
+++ tensorflow/core/grappler/optimizers/auto_mixed_precision_lists.h
@@ -128,6 +128,8 @@ class AutoMixedPrecisionLists {
         "FloorDiv",
         "FusedBatchNormV2",
         "FusedBatchNormGradV2",
+        "FusedBatchNormV3",
+        "FusedBatchNormGradV3",
         "Inv",
         "LeakyRelu",
         "LeakyReluGrad",
